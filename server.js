edecaf26e7e36d5a576e3dec9bd1667dd1ad91b8
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-11 21:46:53 +1100

    add server js to test the application

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
-const apiRoutes = require('./routes/aprRoutes');
-const htmlRoutes =require
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes =require('./routes/htmlRoutes');
 
+// create a html 
+const app = express();
+const PORT =process.env.PORT || 3001;
+
+// parse route middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use('/api, apiRoutes');
+app.use('/', htmlRoutes);
+
+// start the server on port
+
+app.listen(PORT, () => console.log('Listening on PORT: ${PORT}'));
