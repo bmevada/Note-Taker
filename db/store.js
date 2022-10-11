61ae8133f6d934cddfe8e1168d4a61f3be38dbe6
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-11 21:38:20 +1100

    add routes and server js files

---
+++ db/store.js
@@ -0,0 +1,41 @@
+const util = require ('util');
+const fs = require ('fs');
+const uuidv = require ('uuid');
+
+// Read and Write File
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Read, Write and Get Notes
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+    write(note) {
+        return writeFileAsync ('db/db.json', JSON.stringify(note));
+    }
+    // If there are notes
+    getNotes({
+        return this.read().then((notes) => {
+            let parsedNotes;
+            
+            // Otherwise, if there are no notes - send back an empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes =[];
+            }
+            return parsedNotes = [];
+        })
+    })
+}
+
+module.exports = new Store ();
+
+class Store {
+    read () {
+        return
+    }
+    getNotes()
+}
+
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const apiRoutes = require('./routes/aprRoutes');
+const htmlRoutes =require
+
