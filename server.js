fef0d47b5b50cb9bc7c9f59a6f40a32a86129689
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-11 22:51:50 +1100

    add comments

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const htmlRoutes =require('./routes/htmlRoutes');
 const app = express();
 const PORT =process.env.PORT || 3001;
 
-// parse route middleware
+// Parse URL encoded & JSON
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+// Host public folder
 app.use(express.static('public'));
+
+// Use apiRoutes
 app.use('/api, apiRoutes');
 app.use('/', htmlRoutes);
 
 // start the server on port
 
-app.listen(PORT, () => console.log('Listening on PORT: ${PORT}'));
+app.listen(PORT, () => {
+    console.log('API server now on PORT: ${PORT}');
+});
